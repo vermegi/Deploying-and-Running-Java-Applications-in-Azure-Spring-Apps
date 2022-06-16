a6eed5e43a36cdb700037b700e1d7e43d34f8220
Gitte Vermeiren <givermei@microsoft.com>
2022-06-16 13:30:48 +0200

    updated starter

--- StarterProject/spring-petclinic-messaging-emulator/src/main/java/org/springframework/samples/petclinic/messagingemulator/web/AzureServiceBusResource.java
+++ StarterProject/spring-petclinic-messaging-emulator/src/main/java/org/springframework/samples/petclinic/messagingemulator/web/AzureServiceBusResource.java
@@ -5,15 +5,20 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.samples.petclinic.messagingemulator.entity.PetClinicMessageRequest;
 import org.springframework.samples.petclinic.messagingemulator.entity.PetClinicMessageResponse;
 import org.springframework.samples.petclinic.messagingemulator.service.PetClinicVisitRequestSender;
+import org.springframework.samples.petclinic.messagingemulator.model.VisitRequest;
+import org.springframework.samples.petclinic.messagingemulator.model.VisitRequestRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +31,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 class AzureServiceBusResource {
     private final PetClinicVisitRequestSender petClinicVisitRequestSender;
+    private final VisitRequestRepository visitRequestRepository;
     
     @PostMapping
     public String postMessage(@RequestBody PetClinicMessageRequest message) {
